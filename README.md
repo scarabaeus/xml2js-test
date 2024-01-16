d173ac769e13f3f63fe68adc5120ddde90bda9df
Steve Gomez <StevenAGomez@Live.com>
2024-01-16 10:58:51 -0800

    Adds xml2js

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ts-node-18-express
+# xml2js
+
+https://www.npmjs.com/package/xml2js
+
+Input:
+
+```xml
+<SOAP-ENV:Body xmlns:m = "http://www.xyz.org/quotation">
+  <m:GetQuotationResponse>
+    <m:Quotation>Here is the quotation</m:Quotation>
+  </m:GetQuotationResponse>
+</SOAP-ENV:Body>
+```
+
+Output:
+
+```json
+{
+  "result": {
+    "SOAP-ENV:Body": {
+      "$": {
+        "xmlns:m": "http://www.xyz.org/quotation"
+      },
+      "m:GetQuotationResponse": [
+        {
+          "m:Quotation": ["Here is the quotation"]
+        }
+      ]
+    }
+  }
+}
+```
+
+Related:
+
+- https://github.com/scarabaeus/xml2js-test
+- https://github.com/scarabaeus/xml-js-test
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import Xml2Js from 'xml2js';
+
 const app = express();
 
+// const xml = `<?xml version="1.0" encoding="utf-8"?>
+//   <note importance="high" logged="true">
+//       <title>Happy</title>
+//       <todo>Work</todo>
+//       <todo>Play</todo>
+//   </note>`;
+
+const xml = `<SOAP-ENV:Body xmlns:m = "http://www.xyz.org/quotation">
+<m:GetQuotationResponse>
+   <m:Quotation>Here is the quotation</m:Quotation>
+</m:GetQuotationResponse>
+</SOAP-ENV:Body>`;
+
 app.get('/', function (req, res) {
-  res.send('ts-node-18-express');
+  Xml2Js.parseString(xml, function (err: any, result: any) {
+    res.json({
+      result: result,
+    });
+  });
 });
 
-app.listen(3000);
+app.listen(8087);
